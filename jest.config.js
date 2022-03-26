d75b335e4e90d5d8014a33f1cc3077e3766062bb
helen.thu.huong <helen.thu.huong@luxuryescapes.com>
2022-03-26 16:26:50 +1300

    config jest

---
+++ jest.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  // Jest transformations -- this adds support for TypeScript
+  // using ts-jest
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest'
+  },
+
+  // Test spec file resolution pattern
+  // Matches parent folder `__tests__` and filename
+  // should contain `test` or `spec`.
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+
+  // Module file extensions for importing
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__mocks__/fileMock.js',
+    '\\.(css|less)$': '<rootDir>/jest/__mocks__/styleMock.js'
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest/setupTests.ts']
+};
